ef5b105d5b13f76c46d5a80119172f7e67c5acfc
cnwv <115669384+cnwv@users.noreply.github.com>
2024-07-07 23:59:53 +0300

    add naming convention

--- backend/core/models/base.py
+++ backend/core/models/base.py
@@ -1,13 +1,15 @@
 from sqlalchemy import MetaData
 from sqlalchemy.orm import (
     DeclarativeBase, declared_attr)
+from core.config import settings
+from utils import camel_case_to_snake_case
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
 
     metadata = MetaData(
-        naming_convention=...  # TODO: Add naming convention
+        naming_convention=settings.db.naming_convention
     )
 
     @declared_attr.directive
