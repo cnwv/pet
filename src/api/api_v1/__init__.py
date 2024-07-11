09aef23f8bbc2ab28043b5afe2bc3269ae65a36b
cnwv <115669384+cnwv@users.noreply.github.com>
2024-07-11 17:23:37 +0300

    remove useless endpoints

--- src/api/api_v1/__init__.py
+++ src/api/api_v1/__init__.py
@@ -1,25 +1,13 @@
 from fastapi import APIRouter
 
-from core.config import settings
-
-from .users import router as users_router
 from .films import router as films_router
 from core.config import settings
 
 router = APIRouter(
     prefix=settings.api.v1.prefix,
 )
-router.include_router(
-    users_router,
-    prefix=settings.api.v1.users,
-)
 
 router.include_router(
     films_router,
     prefix=settings.api.v1.films,
 )
-
-
-@router.get("/check_env")
-async def check_env():
-    return settings.run.env_variable
