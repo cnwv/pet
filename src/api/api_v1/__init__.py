682ca1eda9568e192367ff52c5f326b8f8a59a05
cnwv <115669384+cnwv@users.noreply.github.com>
2024-07-30 00:03:54 +0300

    demo cookies set and read

--- src/api/api_v1/__init__.py
+++ src/api/api_v1/__init__.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from .films import router as films_router
 from core.config import settings
 from api.api_v1.demo_auth import router as demo_auth_router
+from api.api_v1.demo_auth.test_cookies import router as cookie_router
 
 router = APIRouter(
     prefix=settings.api.v1.prefix,
@@ -12,8 +13,11 @@ router.include_router(
     films_router,
     prefix=settings.api.v1.films,
 )
+router.include_router(
+    cookie_router,
+    prefix="/cookie",
+)
 
 router.include_router(
     demo_auth_router,
 )
-
